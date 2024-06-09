f8c21923fa5f1cf1df8b97220c759dfa9b669f43
fardin379 <nrdsraz@gmail.com>
2024-06-10 02:48:19 +0530

    deleted

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { Image, StyleSheet, View,Text } from 'react-native'
+
+
+export default function HomeScreen () {
+  return (
+  <>
+  <View style={styles.titleContainer}>
+    <Text>ghj</Text>
+  </View>
+  </>
+  )
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flex:1,
+    alignItems: 'center',
+    marginTop:30,
+    backgroundColor:'pink'
+  },
+})
